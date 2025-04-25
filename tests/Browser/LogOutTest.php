ed39f8f9ddc03b7c6d9afc55a8f87ea09540894f
aninfbrnn <anindhita2502@gmail.com>
2025-04-25 21:08:19 +0700

    logout
    ANIN

---
+++ tests/Browser/LogOutTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LogOutTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('http://127.0.0.1:8000/')
+                    ->clickLink('Log in')
+                    ->assertPathIs('/login')
+                    ->type('email', 'anindhita2502@gmail.com')
+                    ->type('password', '12345678')
+                    ->press('LOG IN')
+                    ->assertPathIs('/dashboard')
+                    ->click('#click-dropdown')
+                    ->clickLink('Log Out')
+                    ->assertPathIs('/');
+        });
+    }
+}
